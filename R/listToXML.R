4fc9ed8c597d05eb0e6b0529d241a7cf6497a373
ceparman <craig.e.parman@gmail.com>
2015-03-17 11:10:41 -0400

    list to XML functions added

---
+++ R/listToXML.R
@@ -0,0 +1,25 @@
+#'Create  XML from a list object for a custom HCP annotation
+#'\code{listToXML} Creates encoded authorization string using base64enc and digest packages
+#'@param node name of root node in XML
+#'@param list object to convert
+#'@return XML object
+#'@export
+#'@examples
+#'\dontrun{listToXML("rootname",list(field1="a",field2="b"))}
+#
+
+listToXML <- function(rootname, sublist){
+  node<- newXMLNode(rootname)
+  for(i in 1:length(sublist)){
+    child <- newXMLNode(names(sublist)[i], parent=node);
+    
+    if (typeof(sublist[[i]]) == "list"){
+      listToXML(child, sublist[[i]])
+    }
+    else{
+      xmlValue(child) <- sublist[[i]]
+    }
+   } 
+ node}
+
+
